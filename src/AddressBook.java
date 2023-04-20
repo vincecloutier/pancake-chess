e8f17ac8eddc2df9b511d100624df664419f2eac
vincecloutier <vincentcloutier@mail.com>
2023-04-20 15:49:00 -0400

    added add / remove contact methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,7 +2,29 @@ import java.util.ArrayList;
 
 public class AddressBook {
     private ArrayList<Contact> contacts;
-    public static void main(String[] args) {
 
+    public AddressBook() {
+        this.contacts = new ArrayList<Contact>();
     }
+
+    public void addContact(Contact contact) {
+        this.contacts.add(contact);
+    }
+
+    public void removeContact(Contact contact) {
+        this.contacts.remove(contact);
+    }
+
+
+
+
+
+
+
+
+
+
+//    public static void main(String[] args) {
+//
+//    }
 }
