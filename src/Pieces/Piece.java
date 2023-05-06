b1db064ad08a7734237d852e15b2a7c7fe5d4743
vincecloutier <vincentcloutier@mail.com>
2023-05-06 07:53:45 -0400

    started piece class and setup basic file

---
+++ src/Pieces/Piece.java
@@ -0,0 +1,23 @@
+package Pieces;
+import Game.*;
+// Created by Vince Cloutier on May 6, 2023.
+public abstract class Piece {
+    public int x, y;
+    public Player player;
+
+    /* Constructor for a Piece
+     @param x is the x location of the Piece
+     @param y is the y location of the Piece
+     @param player is the PLayer object associated with the Piece
+     */
+    public Piece (int x, int y, Player player) {
+        this.x = x;
+        this.y = y;
+        this.player = player;
+
+    }
+
+    public abstract boolean isValidPath (int finalX, int finalY);
+
+
+}
