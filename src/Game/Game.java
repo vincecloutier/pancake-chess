4db8d5a097db4676cff545f0f4c7fe2cf58a005f
vincecloutier <vincentcloutier@mail.com>
2023-05-08 09:41:41 -0400

    currently creating a new game

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,4 +1,22 @@
 package Game;
 
+/**
+ * Created by Anne on 9/11/2014.
+ */
 public class Game {
+
+    public Board gameBoard;
+    public Player player1, player2;
+    public int turn;
+    public boolean invalid = false, capture = false;
+
+    // this constructor sets up a standard game with two players
+    // where player1 is white and player2 is black
+    public Game() {
+        gameBoard = new Board(this);
+        player1 = new Player(this, true, true);
+        player2 = new Player(this, false, false);
+        gameBoard.setPieces();
+        turn = 1;
+    }
 }
