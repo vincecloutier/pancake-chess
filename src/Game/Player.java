b5532ba2210d600f38238b65eaaf2147fd1a1537
vincecloutier <vincentcloutier@mail.com>
2023-05-08 09:41:50 -0400

    added player info to pass in

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,4 +1,22 @@
 package Game;
+import Pieces.*;
 
 public class Player {
+    public Game myGame;
+    public boolean isWhite;
+    public boolean goesFirst;
+    public boolean isLoser = false;
+    public boolean isTurn = false;
+    public int score = 0;
+
+    /* The constructor for a Player.
+    @param isWhite says if the player is white
+    @param goesFirst says if the player goes first
+    */
+    public Player(Game myGame, boolean isWhite, boolean goesFirst) {
+        this.myGame = myGame;
+        this.isWhite = isWhite;
+        this.goesFirst = goesFirst;
+    }
+
 }
