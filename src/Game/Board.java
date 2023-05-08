16f5ef29374c37cb8f14dc1e4a59e44eec623595
vincecloutier <vincentcloutier@mail.com>
2023-05-08 08:59:32 -0400

    started board

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -1,5 +1,13 @@
 package Game;
+import Pieces.*;
 
 public class Board {
+    private Piece[][] board; // the board itself, represented as a 2D array of ChessPiece objects
+
+    public Board() {
+        board = new Piece[8][8]; // it's a standard 8 x 8 chess board
+
+    }
+
 
 }
