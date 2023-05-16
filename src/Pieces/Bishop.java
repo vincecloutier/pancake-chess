7169283ae439a45e79e274d4c8d451b697a6f70f
vincecloutier <vincentcloutier@mail.com>
2023-05-16 09:26:30 -0400

    refactoring

--- src/Pieces/Bishop.java
+++ src/Pieces/Bishop.java
@@ -34,11 +34,9 @@ public class Bishop extends Piece {
      * @param finalY the final Y location
      * @return a boolean indicating whether the path is valid
      */
-    public boolean isValidPath(int finalX, int finalY)
-    {
+    public boolean isValidPath(int finalX, int finalY) {
         int x_diff = Math.abs(finalX - this.x);
         int y_diff = Math.abs(finalY - this.y);
-
         return x_diff == y_diff;
     }
 
@@ -50,22 +48,19 @@ public class Bishop extends Piece {
      * @param finalY the final Y location
      * @return an array of coordinates of the given path
      */
-    public int [][] drawPath(int startX, int startY, int finalX, int finalY)
-    {
-        int pairs = Math.abs(finalX - startX); //length of path
+    public int [][] drawPath(int startX, int startY, int finalX, int finalY) {
+        int pairs = Math.abs(finalX - startX); // length of path
 
-        int x_dir = 1, y_dir = 1; //determining direction
+        int x_dir = 1, y_dir = 1; // determining direction
         if(finalX - startX < 0)
             x_dir = -1;
         if(finalY - startY < 0)
             y_dir = -1;
 
-        int [][] path = new int[2][pairs - 1]; //populating array of pairs
+        int [][] path = new int[2][pairs - 1]; // populating array of pairs
 
-        if(pairs - 1 > 0)
-        {
-            for(int i = 0; i < pairs - 1; i++)
-            {
+        if(pairs - 1 > 0) {
+            for(int i = 0; i < pairs - 1; i++) {
                 path[0][i] = startX + x_dir;
                 path[1][i] = startY + y_dir;
             }
